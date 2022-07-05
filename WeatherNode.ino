624762754b883c2bd859cc287c3b95c2f63d2e43
Hans Müller <hdjm01gmail.com>
2022-07-05 18:57:21 +0200

    Resolve #15

--- WeatherNode.ino
+++ WeatherNode.ino
@@ -1,13 +1,13 @@
-
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 #include <BME280.h>
 #include <BME280I2C.h>
 #include <Wire.h>
+#include <PubSubClient.h>
 
-#define sda D2
-#define scl D1
+#define sda D1
+#define scl D2
 
 String inputString = "";    
 bool stringComplete = false;
@@ -37,44 +37,57 @@ char* NAME = "WeatherNode";
 ESP8266WebServer server(80);
 BME280I2C bme(settings);
 
+//MQTT
+
+WiFiClient wclient;
+
+const char* MQTTClientName   = "ESp-"+ESP.getChipId();
+IPAddress     mqtt_server(192, 168, 178, 24);
+PubSubClient  mqtt_client(wclient);
+int           mqtt_port                 = 1883;
+bool          mqtt_connected            = false;
+int           mqtt_pubtime              = 60 * 1000 * 5;
+int           reconnectMQTT_TRY         = 3;
+unsigned long lastReconnectMQTT         = 0;
+int           reconnectMQTTTime         = 10000;
+int           reconnectMQTT_MAX_TRY     = 100;
+
+long          mqtt_lastpub = 0;
+
+char          clientid[25];
+uint32_t      chipid;
+
+
 String getBME280(void) {
     String json = "{ ";
+    // Node root
     json += "\"name\": \"WeatherNode\", ";
-    
+
+    // root/Sensor
     json += " \"BME280\" : {";
-        
-    json += " \"Temperatur\":";
-    
+
+    // root/sensor/temp/
+    json += " \"Temperatur\":";    
     json += " { ";
-    
     json += "\"value\" : \"" + String(temp) + "\"";
     json += ", \"unit\" : ";
     json += "\"°C\"";
-    
     json += " }";
     
-    json += ", \"Luftfeuchte\":";
-    
-    json += " {";
-    
+    json += ", \"Luftfeuchte\":";    
+    json += " {";    
     json += "\" value\" : ";
-    json += "\"" + String(hum) + "\"";
-    
+    json += "\"" + String(hum) + "\"";    
     json += ", \"unit\" : ";
-    json += "\"%\"";
-    
+    json += "\"%\"";    
     json += " }";
     
-    json += ", \"Luftdruck\":";
-    
-    json += " {";
-    
+    json += ", \"Luftdruck\":";    
+    json += " {";    
     json += " \"value\" : ";
-    json += "\"" + String(pres / 100) + "\"";
-    
+    json += "\"" + String(pres / 100) + "\"";    
     json += ", \"unit\" : ";
-    json += "\"hPa\"";
-    
+    json += "\"hPa\"";    
     json += " }";    
     
     json += " }";    
@@ -83,9 +96,11 @@ String getBME280(void) {
     return json;
 }
 
+
 void handleJson(){
     server.send(200, "application/json", getBME280());
 }
+
 
 void handleNotFound() {
   String message = NAME;
@@ -103,6 +118,27 @@ void handleNotFound() {
   }
 
   server.send(404, "text/plain", message);
+}
+
+
+void  publischBME280(void) {
+  
+    Serial.println("MQTT Publish");
+    
+    static char outstr[15];
+    char topic[80];
+    
+    sprintf(topic,"/ESP-%06X/%s",chipid,"temp");
+    dtostrf(temp,7, 1, outstr);
+    mqtt_client.publish(topic, outstr);
+
+    sprintf(topic,"/ESP-%06X/%s",chipid,"hum");
+    dtostrf(hum,7, 0, outstr);
+    mqtt_client.publish(topic, outstr);
+    
+    sprintf(topic,"/ESP-%06X/%s",chipid,"pres");
+    dtostrf(pres / 100,7, 1, outstr);
+    mqtt_client.publish(topic, outstr);    
 }
 
 
@@ -150,11 +186,23 @@ void setup() {
   server.onNotFound(handleNotFound);
   Serial.println("start Webserver");
   server.begin();
+
+  // MQTT
+  chipid=ESP.getChipId();
+  snprintf(clientid,25,"ESP-%08X",chipid);
+  Serial.print("Set mqtt server: ");
+  Serial.print(mqtt_server);
+  Serial.print(" on port ");
+  Serial.println(mqtt_port);
+  mqtt_client.setServer(mqtt_server, mqtt_port);
+  Serial.println("start mqtt client");
+  mqtt_client.connect(clientid);
   
   Serial.println("Setup Done");
 }
 
-void loop() {
+
+void loop() { 
   server.handleClient();
 
   // Serial Input/Response
@@ -181,6 +229,15 @@ void loop() {
     timeSinceLastBM = millis();
     bme.read(pres, temp, hum, tempUnit, presUnit);
   }
+
+  if(!mqtt_client.connected()) {
+    reconnectMQTT();
+  }
+  
+  if( mqtt_lastpub == 0 || ( mqtt_lastpub + mqtt_pubtime ) < millis() ){
+     publischBME280();
+     mqtt_lastpub = millis();
+  }
   
 }
 
@@ -206,3 +263,62 @@ void serialEvent() {
     }
   }
 }
+
+void reconnectMQTT(){
+  
+  if(mqtt_client.connected()) {
+    return;
+  }else{
+        
+    // Wartezeit einhalten
+    if (millis() - lastReconnectMQTT >= reconnectMQTTTime){
+      
+      /*
+      Serial.print("(re)connect MQTT " + String(mqtt_server.toString()) + ":" + String(mqtt_port));        
+      Serial.print(", Client ID");
+      Serial.print(clientid); 
+      Serial.print(" "); 
+      */
+      
+      if(mqtt_client.connect(clientid)){
+        //Serial.println("connected");
+        reconnectMQTT_TRY = 0; // Anzahl der Versuche zürück setzen
+        return;
+      }else{
+        delay(100);
+        //Serial.print("not connected : ");
+        //Serial.println(mqtt_client.state());
+
+        // Loop until we're reconnected or reach max
+        int con = 0;
+        while (!mqtt_client.connected() and con <= (reconnectMQTT_TRY - 1) ) {
+          Serial.print("."); 
+          // Attempt to connect
+          if (mqtt_client.connect(clientid)) {
+            //Serial.println(" connected");
+            delay(100);
+            return;
+          } else {
+      
+            /*
+             Serial.print("failed, rc=");
+            Serial.print(mqtt_client.state());     
+            
+            Serial.print(" try again in ");
+            Serial.print(reconnectMQTTTime / 1000);
+            Serial.println(" seconds");*/
+            
+            // Wait reconnectMQTTTime ms before retrying
+            delay(100);
+            con++;
+          }
+        }
+        Serial.print("failed, rc=");
+        Serial.println(mqtt_client.state()); 
+      }
+        
+      lastReconnectMQTT = millis();
+    }  
+    
+  }     
+} 
