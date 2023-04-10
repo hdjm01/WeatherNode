7a8a5a5fe4a4ad5930b54487c8e79c3b80674a8e
hdjm01 <59115829+hdjm01@users.noreply.github.com>
2023-04-10 18:02:31 +0200

    Refactoring FileSystem.h

--- FileSystem.h
+++ FileSystem.h
@@ -6,70 +6,76 @@ const char cfgFileName[] = "WeatherNode.cfg";
 
 bool readCFGFile();
 
+void FileSystemInfo();
+void showFiles();
+
 void initFS(){
 
   Serial.println(F("Inizializing FS..."));
-    if (LittleFS.begin()){
-        Serial.println(F("done."));
-    }else{
-        Serial.println(F("fail."));
-        /*LittleFS.format();
-        initFS();*/
-    }
   
-    // Get all information of your LittleFS
+  if (LittleFS.begin()){
+    Serial.println(F("done."));
+  }else{
+    Serial.println(F("fail."));
+    return;
+  }
+
+  FileSystemInfo();
+  showFiles();
+  
+  if(LittleFS.exists(cfgFileName)){
+    Serial.println("Found configuration file");
+    readCFGFile();
+  }else{
+    Serial.println("Missing configuration file");
+    return;
+  }
+  
+}
+
+void FileSystemInfo(){
+    Serial.println("File System Information");
     FSInfo fs_info;
     LittleFS.info(fs_info);
  
     Serial.println("File sistem info.");
  
-    Serial.print("Total space:      ");
-    Serial.print(fs_info.totalBytes);
-    Serial.println("byte");
+    Serial.printf("Total space:      %d byte\n", fs_info.totalBytes);
+    Serial.printf("Total space used: %d byte\n", fs_info.usedBytes); 
+    Serial.printf("Block size:       %d byte\n", fs_info.blockSize);
+    Serial.printf("Page size:        %d byte\n", fs_info.totalBytes);
  
-    Serial.print("Total space used: ");
-    Serial.print(fs_info.usedBytes);
-    Serial.println("byte");
- 
-    Serial.print("Block size:       ");
-    Serial.print(fs_info.blockSize);
-    Serial.println("byte");
- 
-    Serial.print("Page size:        ");
-    Serial.print(fs_info.totalBytes);
-    Serial.println("byte");
- 
-    Serial.print("Max open files:   ");
-    Serial.println(fs_info.maxOpenFiles);
- 
-    Serial.print("Max path length:  ");
-    Serial.println(fs_info.maxPathLength);
+    Serial.printf("Max open files:   %d\n", fs_info.maxOpenFiles);
+    Serial.printf("Max path length:  %d\n", fs_info.maxPathLength);
  
     Serial.println();
- 
-    // Open dir folder
-    Dir dir = LittleFS.openDir("/");
-    // Cycle all the content
-    while (dir.next()) {
-        // get filename
-        Serial.print(dir.fileName());
-        Serial.print(" - ");
-        // If element have a size display It else write 0
-        if(dir.fileSize()) {
-            File f = dir.openFile("r");
-            Serial.println(f.size());
-            f.close();
-        }else{
-            Serial.println("0");
-        }
+}
+
+void showFiles(){
+  Serial.println("Files");
+  // Open dir folder
+  Dir dir = LittleFS.openDir("/");
+  // Cycle all the content
+  while (dir.next()) {
+    // get filename
+    
+    Serial.print(dir.fileName());
+    
+    Serial.print(" - ");
+    
+    // If element have a size display It else write 0
+    if(dir.fileSize()) {
+      File f = dir.openFile("r");
+      Serial.println(f.size());
+      f.close();
+    }else{
+      Serial.println("0");
     }
-  if(LittleFS.exists(cfgFileName)){
-    Serial.println("Found configuration file");   
   }
-  readCFGFile();
 }
 
 bool readCFGFile(){
+  Serial.println("Read config file");
   File file = LittleFS.open(cfgFileName, "r");
   if (!file) {
     Serial.println("Failed to open data file");
